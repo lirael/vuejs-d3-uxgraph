8df453f59996be47248bcd6747b0a7f5a12d3cdf
Olena Pastushenko <olena@MacBook-Pro-Olena.local>
2017-04-11 11:49:20 +0200

    testin npm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Sparklines from './components/Sparklines.js'
+import Sparklines from './components/Sparklines.vue'
 import Linecharts from './components/Linecharts.js'
 import Barcharts from './components/Barcharts.js'
 
